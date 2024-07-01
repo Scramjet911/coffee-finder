725383ed8233f5ad1023051cdc769568d121c373
Scramjet911 <36035352+Scramjet911@users.noreply.github.com>
2024-07-01 12:41:31 +0000

    Build: (0e2d83e) Fix web and cors errors

--- middleware/corsMiddleware.js
+++ middleware/corsMiddleware.js
@@ -5,7 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const cors_1 = __importDefault(require("cors"));
 const corsOptions = {
-    origin: ["coffee.zephix.org", "localhost:5173" ? process.env.NODE_ENV === 'development' : '']
+    origin: ["https://coffee.zephix.org", "localhost:5173" ? process.env.NODE_ENV === 'development' : '']
 };
 const corsMiddleware = (0, cors_1.default)(corsOptions);
 exports.default = corsMiddleware;
