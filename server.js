0a45d7d0539052f396e591e1e2e85fd051c8dc86
Scramjet911 <36035352+Scramjet911@users.noreply.github.com>
2024-07-01 12:58:30 +0000

    Build: (37d07c2) Fix web and cors errors

--- server.js
+++ server.js
@@ -12,12 +12,12 @@ const logger_1 = __importDefault(require("./utils/logger"));
 const loggerMiddleware_1 = __importDefault(require("./middleware/loggerMiddleware"));
 const corsMiddleware_1 = __importDefault(require("./middleware/corsMiddleware"));
 const app = (0, express_1.default)();
+app.use(corsMiddleware_1.default);
 (0, db_1.default)();
 app.use(body_parser_1.default.json());
 app.use("/api/coffee-shops", coffeeShopRoutes_1.default);
 app.use("/api/coffee-shops", productRoutes_1.default);
 app.use(loggerMiddleware_1.default);
-app.use(corsMiddleware_1.default);
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     logger_1.default.info(`Server is running on port ${PORT}`);
