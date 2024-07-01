ba317c5a77d9ce79703b0980cad4d48f71dc641c
Scramjet911 <36035352+Scramjet911@users.noreply.github.com>
2024-07-01 11:31:34 +0000

    Build: (47eaa8f) Setup cors and fix build errors

---
+++ middleware/corsMiddleware.js
@@ -0,0 +1,11 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const cors_1 = __importDefault(require("cors"));
+const corsOptions = {
+    origin: ["zephix.org", "localhost:5173" ? process.env.NODE_ENV === 'development' : '']
+};
+const corsMiddleware = (0, cors_1.default)(corsOptions);
+exports.default = corsMiddleware;
--- server.js
+++ server.js
@@ -9,11 +9,15 @@ const db_1 = __importDefault(require("./config/db"));
 const coffeeShopRoutes_1 = __importDefault(require("./routes/coffeeShopRoutes"));
 const productRoutes_1 = __importDefault(require("./routes/productRoutes"));
 const logger_1 = __importDefault(require("./utils/logger"));
+const loggerMiddleware_1 = __importDefault(require("./middleware/loggerMiddleware"));
+const corsMiddleware_1 = __importDefault(require("./middleware/corsMiddleware"));
 const app = (0, express_1.default)();
 (0, db_1.default)();
 app.use(body_parser_1.default.json());
 app.use("/api/coffee-shops", coffeeShopRoutes_1.default);
 app.use("/api/coffee-shops", productRoutes_1.default);
+app.use(loggerMiddleware_1.default);
+app.use(corsMiddleware_1.default);
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     logger_1.default.info(`Server is running on port ${PORT}`);
